143bba18e64d914860920bfe3e30f371ea11d361
Tyler Menezes <tylermenezes@gmail.com>
2014-06-03 11:15:10 -0700

    Add lastfm image proxy

--- app/start.php
+++ app/start.php
@@ -1,5 +1,22 @@
 <?php
 
+if ($_GET['proxy']) {
+    if (!preg_match('/^[A-Za-z0-9\-\.]*last\.fm\/[a-zA-Z0-9\-\.\/\_]*\/[A-Za-z0-9\-\_]+\.(jpg|png|gif)$/', $_GET['proxy'])) {
+        exit;
+    }
+
+    $image_type = preg_replace('/^.*\.([a-z]{3})$/', '\1', $_GET['proxy']);
+
+    if (!in_array($image_type, ['png', 'jpg', 'gif'])) {
+        exit;
+    }
+
+    header('Content-type: image/'.$image_type);
+    header("Cache-Control: max-age=2592000");
+    echo file_get_contents('http://'.$_GET['proxy']);
+    exit;
+}
+
 // Load Twig
 require_once(__DIR__.DIRECTORY_SEPARATOR.'Twig'.DIRECTORY_SEPARATOR.'Autoloader.php');
 \Twig_Autoloader::register(true);
@@ -18,8 +35,11 @@ $data['music'] = [
 
 $nowplaying = $lastfm->now_playing();
 if ($nowplaying) {
+    $image = $nowplaying->image[count($nowplaying->image) - 1]->{'#text'};
+    $image = '/?proxy='. urlencode(preg_replace('/^https?:\/\//', '', $image));
+
     $data['music']['nowplaying'] = [
-        'image' => $nowplaying->image[count($nowplaying->image) - 1]->{'#text'},
+        'image' => $image,
         'title' => $nowplaying->name,
         'artist' => $nowplaying->artist->{'#text'},
         'album' => $nowplaying->album->{'#text'}
@@ -29,10 +49,13 @@ if ($nowplaying) {
 $data['music']['topalbums'] = [];
 
 foreach ($lastfm->top_albums('12month') as $album) {
+    $image = $album->image[1]->{'#text'};
+    $image = '/?proxy='. urlencode(preg_replace('/^https?:\/\//', '', $image));
+
     $data['music']['topalbums'][] = [
         'name' => $album->name,
         'artist' => $album->artist->name,
-        'image' => $album->image[1]->{'#text'},
+        'image' => $image,
     ];
 }
 
